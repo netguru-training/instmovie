69aaed850d91c41533570e18af6ce2e908bdb186
Mykola Kondur <mykola.kondur@in4mates.com>
2015-03-14 01:59:57 +0100

    Add AddMowieRefToReviews migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314104731) do
+ActiveRecord::Schema.define(version: 20150314104732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20150314104731) do
     t.integer  "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "movie_id"
   end
+
+  add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -48,4 +51,5 @@ ActiveRecord::Schema.define(version: 20150314104731) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "reviews", "movies"
 end
