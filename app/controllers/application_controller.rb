280cf38add668d3d4745b6711a2429e428f83c37
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2015-03-14 14:33:52 +0100

    not needed anymore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,26 +4,5 @@ class ApplicationController < ActionController::Base
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
-  
-  def index
-  end
-
-  protected
-    def after_sign_in_path_for(resource)
-      check_admin
-    end
-
-    def after_sign_up_path_for(resource)
-      check_admin
-    end
-
-    def check_admin
-      if current_user.admin?
-        users_admin_url
-      else
-        users_profile_url
-      end
-    end
-
 end
 
