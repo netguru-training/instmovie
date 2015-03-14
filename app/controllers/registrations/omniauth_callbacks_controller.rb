a07ef0e2f6767cbcaecda50f03e1d096107948da
mike092 <mr@gmail.com>
2015-03-14 17:05:33 +0100

    added link to connect account to instagram

--- app/controllers/registrations/omniauth_callbacks_controller.rb
+++ app/controllers/registrations/omniauth_callbacks_controller.rb
@@ -1,8 +1,13 @@
 class Registrations::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def instagram
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    @user.update(access_token: env['omniauth.auth']['credentials']['token'])
-    sign_in_and_redirect @user
+    if current_user!= nil
+      current_user.update(access_token: env['omniauth.auth']['credentials']['token'])
+      redirect_to root_path
+    else
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      @user.update(access_token: env['omniauth.auth']['credentials']['token'])
+      sign_in_and_redirect @user
+    end
     #current_user.update(access_token: env['omniauth.auth']['credentials']['token'])
   end
 end
