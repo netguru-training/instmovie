9a3a5fd5f7a12094050fe9bd1de264c297cf3489
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-03-14 11:16:55 +0100

    syntax error fixed

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,9 @@ module Instmovie
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-  config.generators do |g|
-    g.test_framework :rspec
-    g.template_engine :haml
+    config.generators do |g|
+      g.test_framework :rspec
+      g.template_engine :haml
+    end
   end
 end
