4de673c4515fec89dbcd660b392172fa7d7accff
Krzysztof Kołek <krzysiek.koek6@gmail.com>
2015-03-14 13:02:44 +0100

    Remove binding pry again.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,6 @@ class ApplicationController < ActionController::Base
       end
     end/
     def after_sign_in_path_for(resource)
-      binding.pry
       if current_user.admin?
         redirect_to users_admin_path
       else
