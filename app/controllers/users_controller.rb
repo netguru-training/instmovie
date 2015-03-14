713b7d693eae7dbcf1c172ce09e2f1b36608a734
Krzysztof Kołek <krzysiek.koek6@gmail.com>
2015-03-14 16:02:14 +0100

    Add expose to users controller. Welcome link lead to apprioprate profile page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,5 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
-  def profile
-  end
-
-  def admin
-    @movies = Movie.all
-    @reviews = Review.all
-  end
+  expose(:movies)
+  expose(:reviews)
 end
