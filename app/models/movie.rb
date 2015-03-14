206671930207eb3ea5f01badbee7000c9dcbcb3a
Mykola Kondur <mykola.kondur@in4mates.com>
2015-03-14 02:04:44 +0100

    Add validation to Movie & Review

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
   has_many :reviews
 
+  validates :title , presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :movie
-  
+
+  validates :rating, :content, presence: true
 end
