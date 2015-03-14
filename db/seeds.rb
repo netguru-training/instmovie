a8b2e60bec6a12b265cda16afc3baca4121521db
Krzysztof Kołek <krzysiek.koek6@gmail.com>
2015-03-14 14:59:26 +0100

    Change method to create seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# Admin
-admin = User.create!(email: "admin@netguru.co", password: "adminadmin", password_confirmation: "adminadmin", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: true)
-Movie.create!(title: 'Godfather')
-Movie.create!(title: 'The Shawshank Redemption')
-Movie.create!(title: 'The Lord of the Rings: The Fellowship of the Ring')
-Movie.create!(title: 'The Dark Knight')
-Movie.create!(title: 'Fight club')
-Movie.create!(title: 'Inception')
-Movie.create!(title: 'Rambo')
-Movie.create!(title: 'The Silence of the Lambs')
-Movie.create!(title: 'Se7en')
-Movie.create!(title: 'Saving Private Ryan')
+admin = User.first_or_create(email: "admin@netguru.co", password: "adminadmin", password_confirmation: "adminadmin", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: true)
+Movie.first_or_create(title: 'Godfather')
+Movie.first_or_create(title: 'The Shawshank Redemption')
+Movie.first_or_create(title: 'The Lord of the Rings: The Fellowship of the Ring')
+Movie.first_or_create(title: 'The Dark Knight')
+Movie.first_or_create(title: 'Fight club')
+Movie.first_or_create(title: 'Inception')
+Movie.first_or_create(title: 'Rambo')
+Movie.first_or_create(title: 'The Silence of the Lambs')
+Movie.first_or_create(title: 'Se7en')
+Movie.first_or_create(title: 'Saving Private Ryan')
