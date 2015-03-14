fbab4573e029aadc4f51271add8cc5409ef063bb
Krzysztof Kołek <krzysiek.koek6@gmail.com>
2015-03-14 13:04:02 +0100

    Remove unnecessary code from application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,10 @@
 class ApplicationController < ActionController::Base
-  #after_action :redirect_to_apprioprate_profile, if: :devise_controller? && :devise
   protect_from_forgery with: :exception
 
   def index
   end
 
   protected
-    /def redirect_to_apprioprate_profile
-      if current_user.admin?
-        redirect_to users_admin_path
-      else
-        redirect_to users_profile_path
-      end
-    end/
     def after_sign_in_path_for(resource)
       if current_user.admin?
         redirect_to users_admin_path
