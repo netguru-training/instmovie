e27cd51a664e0ef25d9ec3a0e94970f1178d3857
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-03-14 13:22:53 +0100

    DB seed for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 # Admin
 admin = User.create!(email: "admin@netguru.co", password: "adminadmin", password_confirmation: "adminadmin", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: true)
+
