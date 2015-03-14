e5ca595dbc53fa6108bc2aed3431079045495029
Mykola Kondur <mykola.kondur@in4mates.com>
2015-03-14 02:45:36 +0100

    Add expose configuration to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  decent_configuration do
+    strategy DecentExposure::StrongParametersStrategy
+  end
+  
   def index
   end
 
@@ -22,3 +26,4 @@ class ApplicationController < ActionController::Base
     end
 
 end
+
