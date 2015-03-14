dc5e90cea3ecb82548b57aaecb3fe1eb95ce736e
Mykola Kondur <mykola.kondur@in4mates.com>
2015-03-14 01:23:50 +0100

    Add Movie resource

---
+++ spec/views/movies/edit.html.haml_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "movies/edit", type: :view do
+  before(:each) do
+    @movie = assign(:movie, Movie.create!(
+      :title => "MyString"
+    ))
+  end
+
+  it "renders the edit movie form" do
+    render
+
+    assert_select "form[action=?][method=?]", movie_path(@movie), "post" do
+
+      assert_select "input#movie_title[name=?]", "movie[title]"
+    end
+  end
+end
---
+++ spec/views/movies/index.html.haml_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "movies/index", type: :view do
+  before(:each) do
+    assign(:movies, [
+      Movie.create!(
+        :title => "Title"
+      ),
+      Movie.create!(
+        :title => "Title"
+      )
+    ])
+  end
+
+  it "renders a list of movies" do
+    render
+    assert_select "tr>td", :text => "Title".to_s, :count => 2
+  end
+end
