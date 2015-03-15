fe1744b8c98fdbc13e237fdcdf100c4fe3a33b94
Mykola Kondur <mykola.kondur@in4mates.com>
2015-03-15 07:02:54 +0100

    add routing for suggested films

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,16 @@ Rails.application.routes.draw do
 
   get 'users/profile'
   get 'users/admin'
+
   get 'movies/:id/add_tags', to: 'movies#add_tags', as: 'add_tags'
   #post 'movies/:id/create_tags', to: 'movies_create_tags', as: 'create_tags'
   get 'movies/:id/edit_tags', to: 'movies#edit_tags', as: 'edit_tags'
   #put 'movies/:id/update_tags', to: 'movies_update_tags', as: 'update_tags'
 
   get 'tags/ajax_get_list' => 'tags#ajax_get_list'
+
+  get 'suggested_films', to: 'users#suggested_films'
+
 
   get 'search/movies' => 'movies#search'
 
