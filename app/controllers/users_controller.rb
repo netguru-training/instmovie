0f7eba2c053de4ef505d1332abf3f82fe1c54ff4
Krzysztof Kołek <krzysiek.koek6@gmail.com>
2015-03-15 12:03:20 +0100

    Remove empty actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def profile
-  end
-
-  def admin
-
-  end
-
   expose(:movies) {
     Movie.order("created_at DESC").all
   }
   expose(:reviews) {
     Review.order("created_at DESC").all
   }
-
-  def profile
-  end
-
-  def admin
-  end
 
   def omniauth_callbacks
 
