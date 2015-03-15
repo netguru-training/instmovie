c0dd551f964c5a77a3dd0a5c794749f5935972bc
Mykola Kondur <mykola.kondur@in4mates.com>
2015-03-15 04:39:31 +0100

    Migration - add image to Movie

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315091021) do
+ActiveRecord::Schema.define(version: 20150315091022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150315091021) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.string   "image"
   end
 
   create_table "overall_averages", force: :cascade do |t|
